d8a3432f1a0ff91c3422cf136e5ac3144bac8a08
Omar Al-Ezzi <ishtaar.it@gmail.com>
2022-01-17 21:41:00 +0100

    weatherApp v0.2 constract the weather for Düs city from the openweathermap api and print temp and weatherDescription in console

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-// weather application 
+// weather application
 // express server
-const express = require('express'); 
+const express = require("express");
 // https module
-const https = require('https');
+const https = require("https");
 
 // Create a new express application instance
-const app = express(); 
+const app = express();
 
 // api link for openweathermap
 const url =
-  "https://api.openweathermhttps://api.openweathermap.org/data/2.5/weather?q=Duesseldorf,germany&appid=e747450ae79de538a4c9c6149a492561&units=metricap.org/data/2.5/weather?q=London,uk&appid=e747450ae79de538a4c9c6149a492561";
+  "https://api.openweathermap.org/data/2.5/weather?q=Duesseldorf,germany&appid=e747450ae79de538a4c9c6149a492561&units=metric";
 
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+  // to show the status code from the response of the openweathermap api
+  https.get(url, (resp) => {
+    console.log(resp.statusCode);
 
-app.get('/', (req, res) => {
-    res.send('Server is up and running');
+    // to show the response body pure data before parssing them
+    // resp.on('data', (pureHexaData) => {
+    //   console.log(pureHexaData);
+    // });
 
-    https.get(url, (resp) => {
-    console.log(resp);
-    })
+    // to show the response body pure data after parssing them
+    resp.on("data", (pureHexaData) => {
+      // console.log(JSON.parse(pureHexaData));
+      const weatherData = JSON.parse(pureHexaData);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(weatherDescription);
+      
+
+    });
+  });
 });
-
-
-
 
 // The port the express app will listen on
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-}
-);
+  console.log("Example app listening on port 3000!");
+});
